795a3a758d96af1f80a361496a0fdf8bbef4d916
Heru Aryasuta <heru.210180105@mhs.unimal.ac.id>
2023-12-06 15:56:14 +0700

    add CardView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("io.coil-kt:coil-compose:2.2.0")
+    implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
---
+++ app/src/main/java/com/dylan/friendease/ui/components/CardView.kt
@@ -0,0 +1,147 @@
+package com.dylan.friendease.ui.components
+
+import androidx.cardview.widget.CardView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dylan.friendease.R
+
+
+@Composable
+fun CardView(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(15.dp)
+            )
+            .padding(4.dp)
+            .shadow(4.dp, shape = RoundedCornerShape(15.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.tos),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(68.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Top)
+                ) {
+                    Text(
+                        text = "Fujikawa Chiai",
+                        color = Color(0xFF9B274F),
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    Text(
+                        text = "@fuji_chiai",
+                        color = Color(0xFF9B274F),
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+            Text(
+                text = "Saya adalah seorang musisi, bisa bermain gitar, dan pendengar yang ba...",
+                color = Color(0xFF9B274F),
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Tag(text = "Musik", color = Color(0xFFE5A8BF))
+                Spacer(modifier = Modifier.width(8.dp))
+                Tag(text = "Curhat", color = Color(0xFFE5A8BF))
+                Spacer(modifier = Modifier.width(8.dp))
+                Tag(text = "Aktor/Aktris", color = Color(0xFFE5A8BF))
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Tag(text = "Penyanyi", color = Color(0xFFE5A8BF))
+                Spacer(modifier = Modifier.width(8.dp))
+                Tag(text = "Penuh semangat", color = Color(0xFFE5A8BF))
+            }
+        }
+    }
+}
+
+@Composable
+fun Tag(text: String, color: Color) {
+    Box(
+        modifier = Modifier
+            .background(
+                color = color,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(4.dp)
+    ) {
+        Text(
+            text = text,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MenuItemPreview(){
+    MaterialTheme{
+        CardView()
+    }
+}
